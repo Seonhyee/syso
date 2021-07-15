9660c9f189c83c230e1d1bbb57d06fb5cd90a951
Kimchi_zeon <Kimchi_zeon@DESKTOP-CO5F34H>
2021-07-15 15:57:40 +0900

    Change login/getKakaoAuthUrl

--- src/main/java/com/itwillbs/controller/MemberController.java
+++ src/main/java/com/itwillbs/controller/MemberController.java
@@ -12,51 +12,47 @@ public class MemberController {
 
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public String join() {
-		
+
 		return "/member/joinForm";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
-		
+
 		return "/member/loginForm";
 	}
-	
+
 	@RequestMapping(value = "/find_Info", method = RequestMethod.GET)
 	public String find_Info() {
-		
+
 		return "/member/find_Info";
 	}
-	
+
 	@RequestMapping(value = "/member/update", method = RequestMethod.GET)
 	public String update() {
-		
+
 		return "/member/updateForm";
 	}
-	
+
 	@RequestMapping(value = "/member/updatePro", method = RequestMethod.GET)
 	public String updatePro() {
-		
+
 		return "/member/updatePro";
 	}
-	
+
 	@RequestMapping(value = "/member/deletePro", method = RequestMethod.GET)
 	public String deletePro() {
-		
+
 		return "/member/deletePro";
 	}
-	
-	@Controller
-	public class KakaoController {
 
-		@RequestMapping(value = "/login/getKakaoAuthUrl")
-		public @ResponseBody String getKakaoAuthUrl(HttpServletRequest request) throws Exception{
-			
-			String reqUrl = kakaoAuthUrl + "/oauth/authorize?client_id=" + kakaoApiKey + "&redirect_uri="+ redirectURI + "&response_type=code";
-			
-			return reqUrl;
-		}
-	
-	
-	
+	@RequestMapping(value = "/login/getKakaoAuthUrl")
+	public @ResponseBody String getKakaoAuthUrl(HttpServletRequest request) throws Exception {
+
+		String reqUrl = kakaoAuthUrl + "/oauth/authorize?client_id=" + kakaoApiKey + "&redirect_uri=" + redirectURI
+				+ "&response_type=code";
+
+		return reqUrl;
+	}
+
 }
