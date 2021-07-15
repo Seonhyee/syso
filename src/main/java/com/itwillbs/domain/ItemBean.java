80a3dd944595f0f23809505f70ce41d6d2a93951
minsu86 <manager.kim86@gmail.com>
2021-07-16 00:14:18 +0900

    7/15 home

---
+++ src/main/java/com/itwillbs/domain/ItemBean.java
@@ -0,0 +1,27 @@
+package com.itwillbs.domain;
+
+public class ItemBean {
+	private int category_idx;
+	
+	private item_idx int(11) AI PK 
+	private member_id varchar(45) 
+	private category_idx int(11) 
+	private item_subject varchar(45) 
+	private item_detail varchar(3000) 
+	private item_img varchar(100) 
+	private item_price int(11) 
+	item_count int(11) 
+	item_isUsed tinyint(1) 
+	item_isExchange tinyint(1) 
+	item_region varchar(45) 
+	item_isDirect tinyint(1) 
+	item_isDeliveryFree tinyint(1) 
+	item_charge int(11) 
+	item_likesCount int(11) 
+	item_readcount int(11) 
+	item_startDate date 
+	item_endDate date 
+	item_state int(11) 
+	item_date timestamp
+	
+}
---
+++ src/main/webapp/WEB-INF/views/category.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<%
+	int category_idx = Integer.parseInt(request.getParameter("category_idx"));
+%>
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>카테고리</h1>
+	
+</body>
+</html>
